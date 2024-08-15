54b72a5e136a24b73d40ae545091de79d9a03ba6
alex <skupov93@gmail.com>
2024-08-15 12:17:32 +0500

    Added pathes for weston, logo for application of tester, setting for weston.

---
+++ board/diasom/rk3568_tester/fs-overlay/etc/profile.d/xdg.sh
@@ -0,0 +1,22 @@
+# Set XDG_RUNTIME_DIR manually.
+# The variable is not set for ssh login, causing app failures. It's not clear
+# if this is an error or by design, but setting it manually does help and
+# doesn't seem to hurt...
+
+if test -z "$XDG_RUNTIME_DIR"; then
+	export XDG_RUNTIME_DIR=/run/weston
+fi
+
+if test -z "$WAYLAND_DISPLAY"; then
+	export WAYLAND_DISPLAY=wayland-1
+fi
+
+if test -z "$WESTON_DRM_PRIMARY"; then
+	export WESTON_DRM_PRIMARY=DSI-1
+fi
+
+if [ ! -d $XDG_RUNTIME_DIR ]; then
+	mkdir -p $XDG_RUNTIME_DIR
+fi
+
+chmod 0700 $XDG_RUNTIME_DIR

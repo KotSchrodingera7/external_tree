a551d6408df73f5885b7bf1db5743cb21cbb34a7
alex <skupov93@gmail.com>
2024-09-21 17:32:04 +0500

    Added new package. Added script for init camera.

---
+++ package/exhibition/exhibition.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# EXPO
+#
+################################################################################
+
+EXPO_VERSION = master
+EXPO_SITE = https://github.com/KotSchrodingera7/study.git
+EXPO_SITE_METHOD = git
+EXPO_INSTALL_STAGING = YES
+
+
+define EXPO_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/local/share/qml
+	mkdir -p $(TARGET_DIR)/usr/local/share/icons
+	$(INSTALL) -D -m 0777 $(@D)/expo_view $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/qml/*.qml $(TARGET_DIR)/usr/local/share/qml
+	$(INSTALL) -D -m 0755 $(@D)/icons/* $(TARGET_DIR)/usr/local/share/icons
+endef
+
+$(eval $(cmake-package))

e7a9b95cbd06bb9fb83d74e514b6953f675d47d2
Alex <skupov93@gmail.com>
2025-08-22 14:34:43 +0500

    Update config and packege of testing for rk3588.

--- package/testing/testing.mk
+++ package/testing/testing.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-TESTING_VERSION = intodaction
+TESTING_VERSION = feature_3588
 TESTING_SITE = https://github.com/KotSchrodingera7/test_peripheral.git
 TESTING_SITE_METHOD = git
 TESTING_INSTALL_STAGING = YES
 
 
 define TESTING_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/local/share/qml
+	mkdir -p $(TARGET_DIR)/usr/local/bin
 	$(INSTALL) -D -m 0777 $(@D)/tester $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755 $(@D)/qml/*.qml $(TARGET_DIR)/usr/local/share/qml
+	$(INSTALL) -D -m 0755 $(@D)/scripts/*.sh $(TARGET_DIR)/usr/local/bin
 endef
 
 $(eval $(cmake-package))

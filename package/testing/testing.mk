bdeb157dabe32529b64c017276b86c140bf5fd2c
Alex <skupov93@gmail.com>
2025-08-28 14:50:47 +0500

    Updated application of testing and kernel.

--- package/testing/testing.mk
+++ package/testing/testing.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TESTING_VERSION = feature_3588
+TESTING_VERSION = 50f4f55
 TESTING_SITE = https://github.com/KotSchrodingera7/test_peripheral.git
 TESTING_SITE_METHOD = git
 TESTING_INSTALL_STAGING = YES
@@ -13,7 +13,7 @@ TESTING_INSTALL_STAGING = YES
 define TESTING_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/bin
 	$(INSTALL) -D -m 0777 $(@D)/tester $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755 $(@D)/scripts/*.sh $(TARGET_DIR)/usr/local/bin
+	$(INSTALL) -D -m 0755 $(@D)/scripts/* $(TARGET_DIR)/usr/local/bin
 endef
 
 $(eval $(cmake-package))

20bf129ac17b9a740ec66e8480635b3607221709
Alex <skupov93@gmail.com>
2025-09-03 16:05:26 +0500

    Update build of testing. Changed setting for weston

--- package/testing/testing.mk
+++ package/testing/testing.mk
@@ -12,8 +12,10 @@ TESTING_INSTALL_STAGING = YES
 
 define TESTING_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/bin
+	mkdir -p $(TARGET_DIR)/usr/local/share
 	$(INSTALL) -D -m 0777 $(@D)/tester $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 0755 $(@D)/scripts/* $(TARGET_DIR)/usr/local/bin
+	$(INSTALL) -D -m 0755 $(@D)/config.cfg $(TARGET_DIR)/usr/local/share
 endef
 
 $(eval $(cmake-package))
